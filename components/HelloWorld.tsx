7aab7430b5f9db4b04b845405b58c050df4c0133
heisanbug <40413513+hei5anbug@users.noreply.github.com>
2021-12-27 15:18:42 +0530

    Update HelloWorld.tsx

--- components/HelloWorld.tsx
+++ components/HelloWorld.tsx
@@ -3,7 +3,7 @@ import * as styles from "./HelloWorld.css";
 export function HelloWorld() {
   return (
     <div className={styles.root}>
-      <h1>🧁 Hello from heisanbug</h1>
+      <h1>🍬 🍬 🍬 🍬 🧁 Hello from lol engineering 🍬 🍬 🍬 🍬 </h1>
     </div>
   );
 }
